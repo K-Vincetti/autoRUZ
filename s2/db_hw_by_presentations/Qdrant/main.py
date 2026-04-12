9767a36071f4a2353a6128a8fb4e8adc79461292
Kirill <petruhinkirill73@gmail.com>
2026-04-12 19:37:20 +0300

    done presentations hw

---
+++ s2/db_hw_by_presentations/Qdrant/main.py
@@ -0,0 +1,11 @@
+﻿from qdrant_client import QdrantClient
+from qdrant_client.models import *
+
+client = QdrantClient(host="localhost", port=6333)
+
+client.create_payload_index("articles", "category", field_schema="keyword")
+client.create_payload_index("articles", "rating", field_schema="float")
+client.create_payload_index("articles", "published_at", field_schema="keyword")
+client.create_payload_index("articles", "views", field_schema="integer")
+
+print("Indexes created")
